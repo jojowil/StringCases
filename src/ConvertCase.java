192d91c9317e07ab7f25280d72626e45a7949d1d
Bill Jojo <72106226+jojowil@users.noreply.github.com>
2025-03-03 12:49:39 -0500

    added documentation.

--- src/ConvertCase.java
+++ src/ConvertCase.java
@@ -1,29 +1,35 @@
 public class ConvertCase {
 
     public static String snakeToCamel(String snakeCase) {
+        // StringBuilder is a nice tool for mutable content.
         StringBuilder s = new StringBuilder();
 
         for (int i = 0; i < snakeCase.length(); i++) {
             char c = snakeCase.charAt(i);
-            /*
-            if (c == '_' && i + 1 < snakeCase.length()) {
-                c = Character.toUpperCase(snakeCase.charAt(++i));
-                s.append(c);
+
+            // This logic demonstrates that the if tests can be combined
+            // and that s.append(c) is unconditional.
+            if (c == '_') {
+                if (i + 1 < snakeCase.length()) {
+                    c = Character.toUpperCase(snakeCase.charAt(++i));
+                    s.append(c);
+                }
             } else
                 s.append(c);
 
-             */
-
-
+            /*
+            // this is the simplified version of the above code.
             if (c == '_' && i + 1 < snakeCase.length())
                 c = Character.toUpperCase(snakeCase.charAt(++i));
             s.append(c);
+             */
 
         }
         return s.toString();
     }
 
     public static String camelToSnake(String camelCase) {
+        // StringBuilder is a nice tool for mutable content.
         StringBuilder s = new StringBuilder();
 
         for (int i = 0; i < camelCase.length(); i++) {
@@ -40,6 +46,7 @@ public class ConvertCase {
         String[] snakes = {"get_token_", "to_uppercase", "to_lowercase", "get_cpl_error", "is_cpl_directive"};
         String[] camels = {"hasNext", "showMessageDialog", "nextInt", "isUpperCase", "indexOf", "isLetterOrDigit"};
 
+        // enhanced for to iterate over the strings.
         for (String snake : snakes)
             System.out.println(snake + " -> " + snakeToCamel(snake));
 
